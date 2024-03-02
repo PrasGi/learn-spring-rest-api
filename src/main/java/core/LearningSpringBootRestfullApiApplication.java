293009360935bb863d4e1b278063de9a1589d96f
Prasetyo Putra Pratama <Prastyopratama01@gmail.com>
2024-03-02 12:33:08 +0700

    feat: first

---
+++ src/main/java/core/LearningSpringBootRestfullApiApplication.java
@@ -0,0 +1,13 @@
+package core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBootRestfullApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringBootRestfullApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/core/LearningSpringBootRestfullApiApplicationTests.java
@@ -0,0 +1,13 @@
+package core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBootRestfullApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
